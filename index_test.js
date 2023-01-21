e3b4f89e204f5f5001bae1a483783b7a1d12ab7a
Fabrice <codinglone@gmail.com>
2023-01-21 22:39:50 +0200

    Modified sum to use let

--- index_test.js
+++ index_test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('Sum', () => {
     it('checks if the sum is equal to 7', () => {
         // Setup
-        const sum = 7;
+        let sum;
 
         // Exercise
         assert.equal(sum, 7);
