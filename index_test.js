8fb7055cab6fbd21444eb986bf517a691ca397ee
Fabrice <codinglone@gmail.com>
2023-01-21 22:38:25 +0200

    Added setup phase

--- index_test.js
+++ index_test.js
@@ -3,6 +3,8 @@ const assert = require('assert');
 
 describe('Sum', () => {
     it('checks if the sum is equal to 7', () => {
+        // Setup
+        const sum = 7;
         assert.equal(sum, 7);
     })
 })
